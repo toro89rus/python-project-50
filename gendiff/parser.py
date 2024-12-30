121635a2092d9dedfecfa5831080c068e8ac2c6d
toro <timofey89rus@gmail.com>
2024-12-30 11:04:22 +0300

    add pathlib in parser.py

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -4,21 +4,24 @@ import yaml
 
 import os.path
 
-
-def get_file_extension(path):
-    return os.path.splitext(path)[1][1:]
+import pathlib
 
 
-def get_file_content(path):
-    with open(path) as file:
+def get_file_extension(filepath):
+    path = pathlib.Path(filepath)
+    return path.suffix.lower()
+
+
+def get_file_content(filepath):
+    with open(filepath) as file:
         content = file.read()
     return content
 
 
 def parse(content, format):
-    if format == "json":
+    if format == ".json":
         return json.loads(content)
-    elif format in ("yaml", "yml"):
+    elif format in (".yaml", ".yml"):
         return yaml.load(content, Loader=yaml.Loader)
     raise TypeError(f"Unsupported file format {format}")
 
